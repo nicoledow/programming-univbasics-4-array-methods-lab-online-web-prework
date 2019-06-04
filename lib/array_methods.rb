9e1f1ed7b0c571aacba609367877552c7e5eee45
Nicole Dow <nikki.dow71@gmail.com>
2019-06-04 16:55:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,13 @@
 def using_include(array, element)
-
+  if array.include?(element)
+    return true
+  else
+    return false
 end
 
 def using_sort(array)
-
+  new_arr = array.sort
+  new_arr
 end
 
 def using_reverse(array)
